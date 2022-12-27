c5cbff4eecaf23ea51279a7c4bdac601a522b399
Sagar Mishra <sagarmish1234@gmail.com>
2022-12-27 22:27:32 +0530

    First commit

--- src/main/app/src/constants.js
+++ src/main/app/src/constants.js
@@ -1,4 +1,4 @@
-const BACKEND_URL =  `http://${window.location.host}`
+const BACKEND_URL =  "http://localhost:8080/firstDeploy"
 // const BACKEND_URL =  `http://localhost:2510`
 const API_URL = BACKEND_URL + "/api"
 
